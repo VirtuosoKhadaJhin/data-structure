c10eed7a4c2b09b5d0629864133f42ad450d606b
felix.zhao <felix.zhao@91nuanyou.com>
2016-12-13 14:56:23 +0800

    点击评论管理报错bug

--- src/main/java/com/nuanyou/cms/entity/enums/Os.java
+++ src/main/java/com/nuanyou/cms/entity/enums/Os.java
@@ -4,7 +4,7 @@ package com.nuanyou.cms.entity.enums;
  *
  */
 public enum Os {
-    Huigou(1, "IOS"),
+    IOS(1, "IOS"),
     Youfu(2, "IPAD"),
     WaiMai(3, "Android"),
     Other(9,"其他");
@@ -31,6 +31,9 @@ public enum Os {
     }
 
     public static Os toEnum(Integer value) {
+
+        if (value == null)
+            return null;
         Os[] values = Os.values();
         for (Os type : values) {
             if (type.value== value.byteValue())
