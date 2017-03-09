87f2a0f8672918c3e40edf10dac107df5fab9d48
alan <alan.ye@91nuanyou.com>
2017-03-09 19:27:02 +0800

    CMS增加商户代金券配置板块

--- src/main/java/com/nuanyou/cms/service/MerchantCardService.java
+++ src/main/java/com/nuanyou/cms/service/MerchantCardService.java
@@ -1,6 +1,8 @@
 package com.nuanyou.cms.service;
 
 import com.nuanyou.cms.entity.MerchantCard;
+import com.nuanyou.cms.util.TimeCondition;
+import org.springframework.data.domain.Page;
 
 /**
  * Created by Felix on 2016/10/9.
@@ -8,5 +10,7 @@ import com.nuanyou.cms.entity.MerchantCard;
 public interface MerchantCardService {
 
     MerchantCard saveNotNull(MerchantCard entity);
+
+    Page<MerchantCard> findByCondition(Integer index, final MerchantCard entity, final TimeCondition validTime);
 
 }
