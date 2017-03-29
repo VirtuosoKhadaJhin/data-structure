b0aa8d1380a6a2b84c6f02286a6e77438e08d91f
alan <alan.ye@91nuanyou.com>
2017-03-29 11:17:30 +0800

    卡券页面样式微调优化 有效期精度修改为年月日

--- src/main/java/com/nuanyou/cms/entity/MerchantCard.java
+++ src/main/java/com/nuanyou/cms/entity/MerchantCard.java
@@ -97,8 +97,8 @@ public class MerchantCard {
         this.explains = explains;
     }
 
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     @Column(name = "validtime", nullable = true)
     public Date getValidTime() {
         return validTime;
