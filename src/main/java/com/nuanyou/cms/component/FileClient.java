9ad5f4946352ed13aea39231de3970faeaa8e842
alan <alan.ye@91nuanyou.com>
2017-05-10 18:42:26 +0800

    图片支持批量上传

---
+++ src/main/java/com/nuanyou/cms/component/FileClient.java
@@ -0,0 +1,34 @@
+package com.nuanyou.cms.component;
+
+import org.joda.time.DateTime;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.ThreadLocalRandom;
+
+public abstract class FileClient {
+
+    abstract String uploadFile(String filePath, File file);
+
+    abstract String uploadFile(String filePath, InputStream is) throws IOException;
+
+    public String uploadFile(File file, String suffix) {
+        return uploadFile(buildPath(suffix), file);
+    }
+
+    public String uploadFile(InputStream is, String suffix) throws IOException {
+        return uploadFile(buildPath(suffix), is);
+    }
+
+    public static String buildPath(String suffix) {
+        StringBuilder filePath = new StringBuilder();
+        filePath.append(new DateTime().toString("yyyy/MM/dd/HH/mm/ss/"));
+        filePath.append(Math.abs(ThreadLocalRandom.current().nextLong()));
+
+        if (!suffix.startsWith("."))
+            filePath.append(".");
+        filePath.append(suffix);
+        return filePath.toString();
+    }
+}
