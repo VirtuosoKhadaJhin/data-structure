b001f078ea0f1e88755702f64a4a7bbdbf5fb8aa
byron.fan <byron.fan@91nuanyou.com>
2017-06-06 19:58:45 +0800

    添加语言国家枚举类型

--- src/main/java/com/nuanyou/cms/controller/LangsDictionaryController.java
+++ src/main/java/com/nuanyou/cms/controller/LangsDictionaryController.java
@@ -2,6 +2,7 @@ package com.nuanyou.cms.controller;
 
 import com.nuanyou.cms.commons.APIResult;
 import com.nuanyou.cms.model.LangsDictionary;
+import com.nuanyou.cms.model.enums.LangsCountry;
 import com.nuanyou.cms.service.LangsDictionaryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -33,6 +34,7 @@ public class LangsDictionaryController {
         Page<LangsDictionary> allDictionary = dictionaryService.findAllDictionary(example);
         model.addAttribute("page", allDictionary);
         model.addAttribute("entity", example);
+        model.addAttribute ( "langsCountries", LangsCountry.values () );
         return "langsDictionary/list";
     }
 
