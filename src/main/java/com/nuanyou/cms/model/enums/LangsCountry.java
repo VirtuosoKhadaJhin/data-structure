3ea3f557435712c865718ac9e261dd35a0e95e5d
秦培力 <neal.qin@91nuanyou.com>
2017-06-13 20:28:25 +0800

    update contractTemplate edit

--- src/main/java/com/nuanyou/cms/model/enums/LangsCountry.java
+++ src/main/java/com/nuanyou/cms/model/enums/LangsCountry.java
@@ -8,14 +8,14 @@ import java.util.List;
  * Created by Byron on 2017/6/6.
  */
 public enum LangsCountry {
-    EN_UK ( 1, "en-US", "英语" ),
-    ZH_CN ( 2, "zh-CN", "中文" ),
-    KO_KR ( 3, "ko-KR", "韩语"),
-    JA_JP ( 4, "ja-JP", "日本" ),
-    DE_DE ( 5, "de-DE", "德语" ),
-    TH ( 6, "th", "泰国" ),
-    En_GB(7, "en-GB", "英国英语" )
-    ;
+    EN_UK(1, "en-US", "英语"),
+    ZH_CN(2, "zh-CN", "中文"),
+    KO_KR(3, "ko-KR", "韩语"),
+    JA_JP(4, "ja-JP", "日本"),
+    DE_DE(5, "de-DE", "德语"),
+    TH_TH(6, "th-th", "泰国"),
+    En_GB(7, "en-GB", "英国英语"),
+    TH(8, "th", "泰国");
 
     private Integer key;
     private String value;
@@ -33,37 +33,37 @@ public enum LangsCountry {
     }
 
     public static LangsCountry toEnum(String value) {
-        for (LangsCountry langsCountry : LangsCountry.values ()) {
-            if (value.equals ( langsCountry.getValue () )) {
+        for (LangsCountry langsCountry : LangsCountry.values()) {
+            if (value.equals(langsCountry.getValue())) {
                 return langsCountry;
             }
         }
-        throw new IllegalArgumentException ( "Cannot create evalue from value: " + value + "!" );
+        throw new IllegalArgumentException("Cannot create evalue from value: " + value + "!");
     }
 
     public static LangsCountry toEnum(Integer key) {
         if (key == null)
             return null;
-        for (LangsCountry langsCountry : LangsCountry.values ()) {
-            if (key == langsCountry.getKey ()) {
+        for (LangsCountry langsCountry : LangsCountry.values()) {
+            if (key == langsCountry.getKey()) {
                 return langsCountry;
             }
         }
-        throw new IllegalArgumentException ( "Cannot create evalue from key: " + key + "!" );
+        throw new IllegalArgumentException("Cannot create evalue from key: " + key + "!");
     }
 
     public static Boolean verifyIsLocalLanguage(String langsDictionary, Integer key) {
-        return langsDictionary.equals ( LangsCountry.ZH_CN.getValue () )
-                || langsDictionary.equals ( LangsCountry.EN_UK.getValue () )
-                || langsDictionary.equals ( LangsCountry.En_GB.getValue () )
-                || langsDictionary.equals ( LangsCountry.toEnum ( key ).getValue () );
+        return langsDictionary.equals(LangsCountry.ZH_CN.getValue())
+                || langsDictionary.equals(LangsCountry.EN_UK.getValue())
+                || langsDictionary.equals(LangsCountry.En_GB.getValue())
+                || langsDictionary.equals(LangsCountry.toEnum(key).getValue());
     }
 
     public static List<LangsCountry> localValues(Integer key) {//
-        List<LangsCountry> langsCountries = Lists.newArrayList ();
-        for (LangsCountry langsCountry : LangsCountry.values ()) {
-            if (key == langsCountry.getKey () || langsCountry.getKey () == 1 || langsCountry.getKey () == 2) {
-                langsCountries.add ( langsCountry );
+        List<LangsCountry> langsCountries = Lists.newArrayList();
+        for (LangsCountry langsCountry : LangsCountry.values()) {
+            if (key == langsCountry.getKey() || langsCountry.getKey() == 1 || langsCountry.getKey() == 2) {
+                langsCountries.add(langsCountry);
             }
         }
         return langsCountries;
