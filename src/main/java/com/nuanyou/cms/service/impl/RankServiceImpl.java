6ff1737470d8826f2408756be02d1666092f0f84
young.gu <young.gu@91nuanyou.com>
2017-06-20 14:37:35 +0800

    screen

--- src/main/java/com/nuanyou/cms/service/impl/RankServiceImpl.java
+++ src/main/java/com/nuanyou/cms/service/impl/RankServiceImpl.java
@@ -5,6 +5,7 @@ import com.nuanyou.cms.entity.Rank;
 import com.nuanyou.cms.model.PageUtil;
 import com.nuanyou.cms.service.RankService;
 import com.nuanyou.cms.util.BeanUtils;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -12,12 +13,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Felix on 2016/9/30.
@@ -39,8 +41,12 @@ public class RankServiceImpl implements RankService {
                 if (entity.getCountry() != null && entity.getCountry().getId() != null)
                     predicate.add(cb.equal(root.get("city").get("country").get("id"), entity.getCountry().getId()));
 
-                if (entity.getCity() != null && entity.getCity().getId() != null)
-                    predicate.add(cb.equal(root.get("city").get("id"), entity.getCity().getId()));
+                if (entity.getObjtype() != null)
+                    predicate.add(cb.equal(root.get("objtype"), entity.getObjtype()));
+    
+    
+                if (entity.getDisplay()!=null)
+                    predicate.add(cb.equal(root.get("display"),entity.getDisplay()));
 
                 Predicate[] pre = new Predicate[predicate.size()];
                 return query.where(predicate.toArray(pre)).getRestriction();
