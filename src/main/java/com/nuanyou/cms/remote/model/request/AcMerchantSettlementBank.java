ae1fc8e9c35a19f5ddb3e07cf2417d713f61cbcb
felix.zhao <felix.zhao@91nuanyou.com>
2017-06-27 10:06:07 +0800

    结算银行打通

---
+++ src/main/java/com/nuanyou/cms/remote/model/request/AcMerchantSettlementBank.java
@@ -0,0 +1,62 @@
+package com.nuanyou.cms.remote.model.request;
+
+/**
+ * Created by nuanyou on 2016/11/2.
+ */
+
+public class AcMerchantSettlementBank {
+    private Long id;
+    private String bankCode;
+    private String branch;
+    private String name;
+    private String account;
+    private Long settlementId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBankCode() {
+        return bankCode;
+    }
+
+    public void setBankCode(String bankCode) {
+        this.bankCode = bankCode;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public Long getSettlementId() {
+        return settlementId;
+    }
+
+    public void setSettlementId(Long settlementId) {
+        this.settlementId = settlementId;
+    }
+}
