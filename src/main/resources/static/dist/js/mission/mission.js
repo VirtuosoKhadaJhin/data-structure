ec3ffea914dff4edbbbd81139de83cf85832802d
mylon.sun <mylon.sun@91nuanyou.com>
2017-06-29 11:11:23 +0800

    update status to "新任务"

--- src/main/resources/static/dist/js/mission/mission.js
+++ src/main/resources/static/dist/js/mission/mission.js
@@ -2,9 +2,51 @@
  * Created by mylon on 2017/6/28.
  */
 $(function () {
+    var mchId;
+    var status;
+
     $(".search-reset").click(function () {
         $(".search-form").find('input:text, input:password, input:file, select, textarea').val('');
         $(".search-form").find(".select2").val('').trigger('change');
+    });
+
+    $(".select-country").on("change", function () {
+        location.href = "list?country=" + $(this).val();
+    });
+
+    $(".approval").on("click", function () {
+        $(".approvalModel").modal('show');
+        mchId = $(this).parent().attr("data-mchId");
+        status = $(".approval-status").val();
+    });
+
+    $(".sure-approval").on("click", function () {
+        var remark = $(".remark-text").val();
+
+        var data = {
+            mchId: mchId,
+            status: status,
+            remark: remark,
+        };
+        $.ajax({
+            url: 'approval',
+            data: JSON.stringify(data),
+            type: 'post',
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (result) {
+                if (result.code == 0) {
+                    $(".approvalModel").modal('hide');
+                    $(".approvalResult-text").text("审批成功！");
+                    $(".approvalResultModel").modal('show');
+                }else{
+                    $(".approvalModel").modal('hide');
+                    $(".approvalResult-text").text(result.msg);
+                    $(".approvalResultModel").modal('show');
+                }
+            }
+        });
+
     });
 
 });
