377a7c1ed08c20c4e8ab176067191e6735814fcb
mylon.sun <mylon.sun@91nuanyou.com>
2017-07-04 13:15:20 +0800

    update

--- src/main/resources/static/dist/js/bd-user/bdUser.js
+++ src/main/resources/static/dist/js/bd-user/bdUser.js
@@ -8,3 +8,38 @@ $(".search-reset").click(function () {
     $(".search-form").find(".select2").val('').trigger('change');
 });
 
+// 删除确认弹窗
+$("#del-bd").on("click", function () {
+    console.log(1);
+    $(".hide-del-id").val($(this).attr("del-id"));
+    $(".deleteModal").modal("show");
+});
+
+// 二次删除确认窗
+$('.sure-del').on("click", function () {
+    $(".deleteModal").modal("hide");
+    $(".secondDeleteModal").modal("show");
+});
+
+// 删除
+$('.second-sure-del').on("click", function () {
+    var id = $(".hide-del-id").val();
+
+    var data = {id: Number(id)};
+    $.ajax({
+        url: 'del',
+        data: JSON.stringify(data),
+        type: 'post',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (result) {
+            $(".secondDeleteModal").modal("hide");
+            if (result.code == 0) {
+                $(".deleteResult").text("删除成功");
+            } else {
+                $(".deleteResult").text(result.msg);
+            }
+            $(".deleteResultModal").modal("show");
+        }
+    });
+});
