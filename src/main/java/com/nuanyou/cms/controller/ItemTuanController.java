13ae4dc72cc47e8dcefa9d747a7c57a69d667494
mylon.sun <mylon.sun@91nuanyou.com>
2017-10-20 17:40:42 +0800

    update bug

--- src/main/java/com/nuanyou/cms/controller/ItemTuanController.java
+++ src/main/java/com/nuanyou/cms/controller/ItemTuanController.java
@@ -2,6 +2,7 @@ package com.nuanyou.cms.controller;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.nuanyou.cms.commons.APIResult;
+import com.nuanyou.cms.component.FileClient;
 import com.nuanyou.cms.dao.ItemCatDao;
 import com.nuanyou.cms.dao.ItemDao;
 import com.nuanyou.cms.dao.ItemTuanDao;
@@ -16,6 +17,7 @@ import com.nuanyou.cms.service.OrderService;
 import com.nuanyou.cms.util.JsonUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,7 +25,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -54,6 +58,25 @@ public class ItemTuanController {
 
     @Autowired
     private ItemService itemService;
+
+    @Autowired
+    @Qualifier("s3")
+    private FileClient fileClient;
+
+    @RequestMapping(value = "upload/editor", consumes = {"multipart/form-data"}, method = RequestMethod.POST)
+    public void editorImgUpload(@RequestParam("imgFile") MultipartFile file, HttpServletResponse response) throws Exception {
+        String fileType = "";
+        String originalFilename = file.getOriginalFilename();
+        if (originalFilename.contains(".")) {
+            fileType = originalFilename.substring(originalFilename.lastIndexOf(".") + 1);
+        }
+        try{
+            String callBackImgUrl = fileClient.uploadFile(file.getInputStream(), fileType);
+            response.getWriter().println("{\"error\" : 0,\"url\" : \"" + callBackImgUrl + "\"}");
+        }catch (Exception e){
+            response.getWriter().println("{\"error\" : 1,\"url\" : \"上传失败，请等待网络稳定时重试！\"}");
+        }
+    }
 
     @RequestMapping(path = {"edit", "add"}, method = RequestMethod.GET)
     public String edit(@RequestParam(required = false) Long id, Model model) {
